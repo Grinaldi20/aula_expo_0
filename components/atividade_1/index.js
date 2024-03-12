a4f4f9e6796f5863270f158370e2afa5e8fa42a6
Grinaldi20 <160647039+Grinaldi20@users.noreply.github.com>
2024-03-12 15:24:02 -0300

    aula 12-03

---
+++ components/atividade_1/index.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+function Atividade1() {
+    return(
+<View style={styles.container}>
+<Text style= {styles.texto}> Atividade1 </Text>
+<Text style= {styles.texto}> Murilo </Text>
+</View>
+    );
+}
+
+export default  Atividade1;
---
+++ components/atividade_1/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+container:{
+    backgroundColor: 'cyan',
+    padding: 10,
+},
+
+texto: {
+    color: 'white',
+    fontSize: 40,
+
+}
+
+
+});
+
+export default styles;
