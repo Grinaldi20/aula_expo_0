dd22fbcc7f6be9665ee52e595d60824d5d2cf94e
Grinaldi20 <160647039+Grinaldi20@users.noreply.github.com>
2024-04-30 15:24:50 -0300

    30-04-24

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import Atividade_1 from './components/atividade_1';
 import Atividade_2 from './components/atividade_2';
+import Exemplo_3 from './components/exemplo_3';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Atividade_2 />
+      <Exemplo_3 />
       <StatusBar style="auto" />
     </View>
   );
@@ -21,5 +23,6 @@ const styles = StyleSheet.create({
     padding:18,
     fontSize:'50px',
     fontFamily:'Copperplate Gothic Bold',
+    justifyContent: 'center',
   },
 });
---
+++ components/Exemplo_3/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#078f91',
+        borderRadius: 20,
+    },
+    titulo: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginVertical: 20,
+        color: '#02e7eb',
+    },
+});
+
+export default styles;
+
